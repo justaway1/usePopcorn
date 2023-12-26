dc70888401ac68f965d5ffb1d12cc3576cdd5c5e
Stanislav Angelov <stannyweb@gmail.com>
2023-12-26 22:18:13 +0200

    Fixes

--- src/index.js
+++ src/index.js
@@ -2,12 +2,63 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import './index.css'
 // import App from './App'
-import StarRating from './StarRating.js'
+// import StarRating from './StarRating.js'
+import TextExpander from './TextExpander.js'
+// import { useState } from 'react'
+
+// function Test () {
+//   const [test, setTest] = useState(0)
+
+//   return (
+//     <div>
+//       <StarRating maxRating={5} onSetRating={setTest} />
+//       <p>This movie is rated {test}</p>
+//     </div>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    {/* <StarRating maxRating={10} />
+    <StarRating maxRating={5} color={'red'} />
+    <StarRating maxRating={8} defaultRating={1} color={'blue'} />
+    <StarRating
+      maxRating={5}
+      color={'green'}
+      messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}
+    />
+    <Test /> */}
+    <TextExpander
+      color={'red'}
+      displayStyle={'inline-block'}
+      truncateLength={20}
+      className='box'
+    >
+      Text Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText
+      Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot
+      of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of
+      TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of
+      TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of
+      TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXT
+    </TextExpander>
+    <TextExpander color={'orange'} displayStyle={'inline'} truncateLength={15}>
+      Text Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText
+      Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot
+      of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of
+      TEXTText Alot of TEXTText Alot of TEXT
+    </TextExpander>
+    <TextExpander
+      text={'Display Me'}
+      color={'blue'}
+      displayStyle={'block'}
+      showed={true}
+    >
+      Text Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText
+      Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot
+      of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of
+      TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXTText Alot of TEXT
+    </TextExpander>
   </React.StrictMode>
 )
